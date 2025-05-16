5c153fca956c88680a2ed4f4dbb57c187ccb4e45
Shiv-Patel-24 <pshiv2411@gmail.com>
2025-05-16 11:16:48 +0530

    mostly footer comp

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,162 @@
-export default function Footer(){
-    return(
-        <>
-            <hr />
-            <h1>This is footer</h1>
-        </>
-    )
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="FooterSmallSize">
+        <div className="Para">
+          <h5>
+            To access and use all Apple Card features and products available
+            only to Apple Card users, you must add Apple Card to Wallet on an
+            iPhone or iPad that supports and has the latest version of iOS or
+            iPadOS. Apple Card is subject to credit approval, available only for
+            qualifying applicants in the United States, and issued by Goldman
+            Sachs Bank USA, Salt Lake City Branch.
+            <br />
+            <br />
+            Apple Payments Services LLC, a subsidiary of Apple Inc., is a
+            service provider of Goldman Sachs Bank USA for Apple Card and
+            Savings accounts. Neither Apple Inc. nor Apple Payments Services LLC
+            is a bank.
+            <br />
+            <br />
+            If you reside in the U.S. territories, please call Goldman Sachs at
+            877-255-5923 with questions about Apple Card. We approximate your
+            location from your internet IP address by matching it to a
+            geographic region or from the location entered during your previous
+            visit to Apple. Apple
+          </h5>
+        </div>
+        <hr />
+        <div className="AllList">
+          <div className="FirstList">
+            <h4>
+              <b>Shop and Learn</b>
+            </h4>
+
+            <li>Store</li>
+            <li>Mac</li>
+            <li>iPad</li>
+            <li>iPhone</li>
+            <li>Watch</li>
+            <li>Vision</li>
+            <li>AirPods</li>
+            <li>TV & Home</li>
+            <li>AirTag</li>
+            <li>Accessories</li>
+            <li>Gift Cards</li>
+
+            <h4><b>Apple Wallet</b></h4>
+            <li>Wallet</li>
+            <li>Apple Card</li>
+            <li>Apple pay</li>
+            <li>Apple Cash</li> 
+          </div>
+          <div className="FirstList">
+            <h4><b>Account</b></h4>
+            <li>Manage Your Apple Account</li>
+            <li>Apple Store Account</li>
+            <li>iCloud.com</li>
+            {/* <br /> */}
+            <h4><b>Entertainment</b></h4>
+            <li>Apple One</li>
+            <li>Apple TV+</li>
+            <li>Apple Music</li>
+            <li>Apple Arcade</li>
+            <li>Apple Podcasts</li>
+            <li>Apple Books</li>
+            <li>App Store</li>
+            
+          </div>
+          <div className="FirstList">
+            <h4><b>Apple Store</b></h4>
+            <li>Find a Store</li>
+            <li>Genius Bar</li>
+            <li>Today at Apple</li>
+            <li>Apple Summer Camp</li>
+            <li>Ways to Buy</li>
+            <li>Apple Trade In</li>
+            <li>Recucling Programme</li>
+            <li>Find your Order - Apple</li>
+            <li>Shopping Help</li>
+            
+          </div>
+          <div className="FirstList">
+            <h4><b>for Business</b></h4>
+            <li>Apple and Business</li>
+            {/* <br /> */}
+            <h4><b>For Education</b></h4>
+            <li>Apple and Education</li>
+            <li>Shop for Education</li>
+            <li>Shop for university</li>
+            {/* <br /> */}
+            <h4><b>For Healthcar</b></h4>
+            <li>Apple in Healthcare</li>
+            <li>Health on Apple Watch</li>
+          </div>
+          <div className="FirstList">
+            <h4><b>Apple Values</b></h4>
+            <li>Accesssibility</li>
+            <li>Environment</li>
+            <li>Privacy</li>
+            <li>Supply Chain</li>
+            {/* <br /> */}
+            <h4><b>About Apple</b></h4>
+            <li>Newsroom</li>
+            <li>Apple Opportunities</li>
+            <li>investors</li>
+            <li>Ethics & Compliance</li>
+            <li>Events</li>
+            <li>Contact Apple</li>
+          </div>
+        </div>
+      </div>
+      <h4 className="LastInfo">More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</h4>
+      <hr />
+      {/* <div className="TotalThreeParts">
+        <div className="FirstPart">
+          <h4>Copyright © 2025 Apple Inc. All rights reserved.</h4>
+        </div>
+        <div className="SecondPart">
+
+        </div>
+        <div className="LastPart">
+          <h5 style={{float:"right"}} >India</h5>
+        </div>
+      </div> */}
+    </div>
+  );
 }
+
+
+{/* <div className="FirstList">
+            <h4>
+              <b>Shop and Learn</b>
+            </h4>
+
+            <li>Store</li>
+            <li>Mac</li>
+            <li>iPad</li>
+            <li>iPhone</li>
+            <li>Watch</li>
+            <li>Vision</li>
+            <li>AirPods</li>
+            <li>TV & Home</li>
+            <li>AirTag</li>
+            <li>Accessories</li>
+            <li>Gift Cards</li>
+
+            <h4><b>Apple Wallet</b></h4>
+            <li>Wallet</li>
+            {/* <li>Apple Card</li>
+            <li>Apple pay</li>
+            <li>Apple Cash</li> 
+            */}
+
+          // </div>
+          // <div className="secondList">
+          //   <h4><b>Account</b></h4>
+          //   <li>Manage Your Apple Account</li>
+          //   <li>Apple Store Account</li>
+          //   <li>iCloud.com</li>
+          // </div> */}
